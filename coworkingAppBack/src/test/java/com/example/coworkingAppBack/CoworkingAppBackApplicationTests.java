11bc2d326ff70e4f8c1d5106be7c76469e461019
marcius02 <marcfernandezz1011@gmail.com>
2025-03-17 12:47:07 +0100

    first commit

---
+++ coworkingAppBack/src/test/java/com/example/coworkingAppBack/CoworkingAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coworkingAppBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoworkingAppBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
